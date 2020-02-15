08e8b8fda0f6dd6ceeeffd9edcedd9fbd5696a0b
Emir Mustafoski <emirmustafoski92@gmail.com>
2020-02-15 10:39:51 +0100

    cleaning the unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 
+/* pages */
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
-  return <div className='App'>helllo from app</div>;
+  return (
+    <div className='App'>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </div>
+  );
 }
 
 export default App;
